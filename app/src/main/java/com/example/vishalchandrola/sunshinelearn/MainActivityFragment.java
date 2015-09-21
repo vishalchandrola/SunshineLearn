efeb7ec209be528b2ea162031fa48f24096022be
vishal chandrola <vdchandrola@gmail.com>
2015-09-21 14:04:20 +0530

    added scroll view with weather data

--- app/src/main/java/com/example/vishalchandrola/sunshinelearn/MainActivityFragment.java
+++ app/src/main/java/com/example/vishalchandrola/sunshinelearn/MainActivityFragment.java
@@ -1,10 +1,16 @@
 package com.example.vishalchandrola.sunshinelearn;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +23,35 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
+        String[] forecastArray ={
+         "ahmedabad - sunny",
+         "vadodara - hot",
+         "surat  - dry",
+         "ahmedabad - sunny",
+         "ahmedabad - sunny",
+         "morbi - thunderstorm",
+         "ahmedabad - sunny",
+         "ahmedabad - sunny",
+                "ahmedabad - sunny",
+                "ahmedabad - sunny",
+                "ahmedabad - sunny",
+                "ahmedabad - sunny",
+        };
+
+
+
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> forecast = new ArrayAdapter<>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
+
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+
+        ListView listview = (ListView)view.findViewById(R.id.listview_forecast);
+        listview.setAdapter(forecast);
+
+        return view;
     }
 }
